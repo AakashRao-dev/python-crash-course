cb4e209ebd9fa4603d2147c127babb65e945f3f9
Aakash <aakash.codes02@gmail.com>
2024-10-21 21:30:00 +0530

    First Python Program

---
+++ ch-2/welcome.py
@@ -0,0 +1,13 @@
+line_01 = "********************"  # header / footer
+line_02 = "*                  *"  # re-use
+line_03 = "*   HELLO WORLD!   *"
+
+line_04 = "**************"
+
+# starts with a blank line
+print('')
+print(line_01)
+print(line_02)
+print(line_03)
+print(line_02)
+print(line_01)
